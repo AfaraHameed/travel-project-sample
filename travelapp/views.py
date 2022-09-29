50b38c2a1dae50446b90364220d64461e79cd150
AfaraHameed <afarasidheek@gmail.com>
2022-09-29 12:04:41 +0530

    updated

--- travelapp/views.py
+++ travelapp/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from . models import Place
 # Create your views here.
 def fun(request):
-    return  render(request,'hai.html',{'name':'afara'})
+    obj = Place()
+    obj.name = "kerala"
+    obj.desc = "God's own country"
+    obj.price = 1000
+    return  render(request,'index.html' , {'result':obj})
+
+def add(request):
+    num1 = int(request.POST['num1'])
+    num2 = int(request.POST['num2'])
+    result = num1+num2
+    return render(request,'result.html',{"result":result})
