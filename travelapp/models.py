50e9c087b9fa9632926c5374b2dc0bb26873a118
AfaraHameed <afarasidheek@gmail.com>
2022-09-30 16:14:18 +0530

    model place created

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Place:
-    id:int
-    name:str
-    img:str
-    desc:str
-    price:int
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='picture')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
